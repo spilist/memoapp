b1c0ca1874993bea445c5a71c8040cb8ef2804a0
Hwidong Bae <spilist@gmail.com>
2018-07-29 00:54:12 +0900

    feat(Spinner): implement Spinner using better-react-spinkit

---
+++ src/components/common/spinner/Spinner.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Flex } from 'reflexbox';
+import { FadingCircle } from 'better-react-spinkit';
+
+class Spinner extends React.Component {
+  static propTypes = {
+    className: PropTypes.string,
+    color: PropTypes.string,
+    size: PropTypes.number,
+  };
+
+  static defaultProps = {
+    className: '',
+    size: 50,
+  };
+
+  render() {
+    return (
+      <Flex justify="center" pt="2rem">
+        <FadingCircle size={this.props.size} color={this.props.color} />
+      </Flex>
+    );
+  }
+}
+
+export default Spinner;
