a30dc89be66c838ba0cdae989515dc85f994f5fa
Hwidong Bae <spilist@gmail.com>
2018-07-28 22:04:25 +0900

    fea(utils): implement MockData with helper functions in TestUtils

--- src/utils/TestUtils.js
+++ src/utils/TestUtils.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { mount, shallow } from 'enzyme';
 import { MemoryRouter, Route } from 'react-router-dom';
+import penderMiddleware from 'redux-pender';
+import configureMockStore from 'redux-mock-store';
 
 const renderConnected = ({ path, store, ConnectedComponent }) => {
   let routingProps;
@@ -42,9 +44,29 @@ const renderSimple = ({ props, Component, deep = false }) => {
 
 const toJS = obj => JSON.parse(JSON.stringify(obj));
 
+const range = num => new Array(num).fill().map((v, i) => i + 1);
+
+// https://gist.github.com/solenoid/1372386
+const mongoObjectId = () => {
+  const timestamp = ((new Date().getTime() / 1000) | 0).toString(16);
+  return (
+    timestamp +
+    'xxxxxxxxxxxxxxxx'
+      .replace(/[x]/g, function() {
+        return ((Math.random() * 16) | 0).toString(16);
+      })
+      .toLowerCase()
+  );
+};
+
+const mockStore = state => configureMockStore([penderMiddleware()])(state);
+
 export default {
   renderConnected,
   renderWithRouter,
   renderSimple,
   toJS,
+  range,
+  mongoObjectId,
+  mockStore,
 };
