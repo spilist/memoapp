f52b0e36056e606a5ba0466db158f66cad59d439
Hwidong Bae <spilist@gmail.com>
2018-07-28 17:21:02 +0900

    feat(root): connect Root component to index instead of App

---
+++ src/components/Root.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
+
+class Root extends Component {
+  render() {
+    return <div className="root">root</div>;
+  }
+}
+
+export default Root;
