90fed2e04472d812602707f88e82a2951574c088
Hwidong Bae <spilist@gmail.com>
2018-07-29 09:34:12 +0900

    feat(index): install fontfaceobserver and set font-face to Spoqa Han Sans

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import 'moment/locale/ko';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { Route, Router } from 'react-router-dom';
+import FontFaceObserver from 'fontfaceobserver';
 import history from './history';
 import store from './store';
 import Root from './components/Root';
@@ -11,6 +12,10 @@ import { ScrollToTop } from './components/common';
 import registerServiceWorker from './registerServiceWorker';
 
 moment.locale('ko');
+
+new FontFaceObserver('Spoqa Han Sans').load(null, 5 * 1000).then(() => {
+  document.body.setAttribute('spoqa-han-sans-loaded', true);
+});
 
 const RouteDeliverer = ({ match, location }) => (
   <Provider store={store}>
