e496a690793f494789bec5e6401303079b1c5d26
Hwidong Bae <spilist@gmail.com>
2018-07-29 09:03:56 +0900

    feat(memoList): add checkboxes to select specific memo or all memos

--- src/components/memo-list/MemoList.js
+++ src/components/memo-list/MemoList.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import { List } from 'immutable';
 import styled from 'styled-components';
-import { Flex, Box } from 'reflexbox';
-import timeUtils from '~/utils/TimeUtils';
 import oc from 'open-color-js';
+import { Flex, Box } from 'reflexbox';
+import { Checkbox } from 'antd';
 import { Spinner } from '../common';
+import timeUtils from '~/utils/TimeUtils';
 
 const MemoHeaderListIcon = styled.div`
   color: ${oc.gray6};
@@ -11,6 +13,14 @@ const MemoHeaderListIcon = styled.div`
 
 const MemoHeaderTitle = styled.div`
   font-weight: bold;
+`;
+
+const MemoHeaderPlusIcon = styled.div`
+  display: flex;
+`;
+
+const MemoCard = styled.div`
+  padding: 8px;
 `;
 
 const MemoCardTitle = styled.div`
@@ -21,8 +31,13 @@ const MemoCardContent = styled.div`
   font-size: 12px;
 `;
 
+const CheckboxWrapper = styled.div`
+  font-size: 12px;
+`;
+
 const initialState = {
   expanded: false,
+  checkedMemos: List(),
 };
 
 export default class MemoList extends Component {
@@ -32,35 +47,91 @@ export default class MemoList extends Component {
     this.setState({ expanded: !this.state.expanded });
   };
 
+  isCheckedAll = () => {
+    const { memos } = this.props;
+    const { checkedMemos } = this.state;
+    if (memos.size === 0) {
+      return false;
+    }
+
+    return checkedMemos.size === memos.size;
+  };
+
+  toggleCheckAll = () => {
+    if (this.isCheckedAll()) {
+      this.setState({ checkedMemos: initialState.checkedMemos });
+    } else {
+      this.setState({
+        checkedMemos: this.props.memos,
+      });
+    }
+  };
+
+  toggleCheckMemo = memo => {
+    const { checkedMemos } = this.state;
+    const index = checkedMemos.findIndex(checked => checked._id === memo._id);
+
+    if (index === -1) {
+      // not checked
+      this.setState({
+        checkedMemos: checkedMemos.push(memo),
+      });
+    } else {
+      this.setState({
+        checkedMemos: checkedMemos.delete(index),
+      });
+    }
+  };
+
   renderHeader = () => {
     const { labelName, memos, loading } = this.props;
     const size = loading ? '...' : memos.size;
     return (
       <Box>
-        <Flex>
-          <MemoHeaderListIcon onClick={this.toggleExpansion}>
-            <i className="fa fa-list" />
-          </MemoHeaderListIcon>
-          <MemoHeaderTitle>{`${labelName} (${size})`}</MemoHeaderTitle>
+        <Flex justify="space-between">
+          <Flex>
+            <MemoHeaderListIcon onClick={this.toggleExpansion}>
+              <i className="fa fa-list" />
+            </MemoHeaderListIcon>
+            <MemoHeaderTitle>{`${labelName} (${size})`}</MemoHeaderTitle>
+          </Flex>
+          <MemoHeaderPlusIcon>
+            <i className="fa fa-plus" />
+          </MemoHeaderPlusIcon>
         </Flex>
+        <CheckboxWrapper>
+          <Checkbox
+            checked={this.isCheckedAll()}
+            onChange={this.toggleCheckAll}
+          >
+            {this.isCheckedAll() ? '모두 선택 해제' : '모두 선택'}
+          </Checkbox>
+        </CheckboxWrapper>
       </Box>
     );
   };
 
   renderList = () => {
     const { memos, loading } = this.props;
+    const { checkedMemos } = this.state;
     return loading ? (
       <Spinner />
     ) : (
       memos.map(memo => (
         <Flex key={memo._id}>
-          <Box>
+          <CheckboxWrapper>
+            <Checkbox
+              checked={checkedMemos.includes(memo)}
+              onChange={() => this.toggleCheckMemo(memo)}
+            />
+          </CheckboxWrapper>
+          <MemoCard>
             <MemoCardTitle>{memo.title}</MemoCardTitle>
             <MemoCardContent>
               {timeUtils.format(memo.updatedAt)}
               {memo.content}
             </MemoCardContent>
-          </Box>
+          </MemoCard>
         </Flex>
       ))
     );
