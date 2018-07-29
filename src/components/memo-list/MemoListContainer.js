aec5819d34f0dac6b5c8050f85d4287c302c5ce8
Hwidong Bae <spilist@gmail.com>
2018-07-29 15:06:06 +0900

    feat(memoList): redirect to the label's first memo when memoSlug is not provided

--- src/components/memo-list/MemoListContainer.js
+++ src/components/memo-list/MemoListContainer.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { matchPath } from 'react-router';
+import textUtils from '~/utils/TextUtils';
 import sortUtils from '~/utils/SortUtils';
 import * as memoListActions from '~/store/modules/memoList';
 import MemoList from './MemoList';
-
-export class MemoListContainer extends Component {
-  render() {
-    return <MemoList {...this.props} />;
-  }
-}
+import history from '~/history';
 
 const labelName = label => {
   switch (label) {
@@ -25,6 +22,40 @@ const memos = (label, memoListState) => {
       return memoListState.memos.sort(sortUtils.byUpdatedAt);
   }
 };
+
+export class MemoListContainer extends Component {
+  componentDidMount() {
+    this.redirect(this.props);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.location.pathname !== prevProps.location.pathname) {
+      this.redirect(this.props);
+    }
+  }
+
+  redirect = props => {
+    const { memos, location } = props;
+    const match = matchPath(location.pathname, {
+      path: '/:labelSlug',
+      exact: true,
+    });
+
+    if (match && match.params && match.params.labelSlug) {
+      if (memos.size > 0) {
+        history.replace({
+          pathname: `/${match.params.labelSlug}/${textUtils.slug(
+            memos.first()
+          )}`,
+        });
+      }
+    }
+  };
+
+  render() {
+    return <MemoList {...this.props} />;
+  }
+}
 
 export default connect(
   ({ memoList, pender }, { label }) => ({
--- src/components/memo-list/__tests__/MemoListContainer.test.js
+++ src/components/memo-list/__tests__/MemoListContainer.test.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { List } from 'immutable';
 import utils from '~/utils/TestUtils';
-import { generateMemos } from '~/__mockdata__/Memo';
-import ConnectedMemoListContainer from '../MemoListContainer';
+import Memo, { generateMemos } from '~/__mockdata__/Memo';
+import ConnectedMemoListContainer, {
+  MemoListContainer,
+} from '../MemoListContainer';
+import history from '~/history';
 
 jest.mock('../MemoList', () => props => (
   <div id="MemoList" props={props}>
@@ -10,13 +13,14 @@ jest.mock('../MemoList', () => props => (
   </div>
 ));
 
-let path, store, ownProps, state, component;
+let path, store, ownProps, simpleProps, state, component;
 beforeEach(() => {
   path = '/all';
   ownProps = {};
+  simpleProps = {};
   state = {
     memoList: {
-      memos: List(generateMemos(15)),
+      memos: List(),
     },
     pender: {
       pending: {},
@@ -33,13 +37,49 @@ const render = () =>
     ConnectedComponent: ConnectedMemoListContainer,
   });
 
+const renderSimple = () =>
+  utils.renderSimple({
+    props: simpleProps,
+    Component: MemoListContainer,
+  });
+
+const renderWithRouter = () =>
+  utils.renderWithRouter({
+    path,
+    props: simpleProps,
+    Component: MemoListContainer,
+  });
+
 describe('[MemoListContainer]', () => {
+  describe('when path does not have memoSlug', () => {
+    describe('when mounted', () => {
+      it('redirects to the first memo if memos exist', () => {
+        path = '/all';
+        simpleProps = {
+          label: 'all',
+          memos: List([
+            new Memo({
+              _id: 'blahblah',
+              title: 'Some random word',
+            }).get(),
+            ...generateMemos(15),
+          ]),
+        };
+        renderWithRouter();
+        expect(history.replace).toBeCalledWith({
+          pathname: '/all/some-random-word--blahblah',
+        });
+      });
+    });
+  });
+
   describe('when props.label === "all"', () => {
     beforeEach(() => {
       ownProps.label = 'all';
     });
 
     it('provides labelName as "전체" and memos as whole memos, which is reverse sorted by updatedAt to MemoList component', () => {
+      state.memoList.memos = List(generateMemos(15));
       component = render();
       const MemoList = component.find('#MemoList');
       expect(MemoList.prop('props').labelName).toBe('전체');
